6229bd7211b94e85e35da35d011f716adc8b820c
Brunoat81 <brunoat@hotmail.co.uk>
2023-03-13 02:34:58 -0300

    Create Pruebacodigo.java

---
+++ DesarrolloUTN/src/Pruebacodigo.java
@@ -0,0 +1,19 @@
+
+public class Pruebacodigo {
+
+	public static void main(String[] args) {
+	
+		String txt = "hola que tal?";
+		
+		"hola que tal?".toUpperCase().replace("hola", "chau");
+		
+		System.out.println (txt);
+		
+		
+		
+				
+		
+
+	}
+
+}
