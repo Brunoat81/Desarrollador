6843cc0fbbb86c0b3dd8bccf9055abafb61755a6
Brunoat81 <brunoat@hotmail.co.uk>
2023-03-13 01:33:03 -0300

    Create Ejercicio4_1a.java

---
+++ DesarrolloUTN/src/Ejercicio4_1a.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Ejercicio4_1a {
+
+	public static void main (String [] args) {char A = 'A';int[] arrAy = new int [] {3, 87, 43};
+		
+        boolean maYormenor = true;
+        
+        if (maYormenor) {
+        	Arrays.sort(arrAy);
+        }
+        System.out.println (A);
+        System.out.println ("De menor a mayor:");
+        
+        for (int numeros : arrAy) {
+        	System.out.println (numeros);
+
+	}
+	}
+}
