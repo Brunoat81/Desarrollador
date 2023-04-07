bec57f8e28ef3c33a3314d47efb3a44e72366fbc
Brunoat81 <101934692+Brunoat81@users.noreply.github.com>
2023-04-07 03:39:50 -0300

    Add files via upload

--- trabajoIntegrador/src/main/java/DesarrolloJava/trabajoIntegrador/tpIntegrador.java
+++ trabajoIntegrador/src/main/java/DesarrolloJava/trabajoIntegrador/tpIntegrador.java
@@ -1,7 +1,7 @@
 package DesarrolloJava.trabajoIntegrador;
 		
 import java.io.FileReader;		
-import java.io.IOException;		
+import java.io.IOException;
 import java.util.List;
 import com.opencsv.bean.CsvToBeanBuilder;
 		
@@ -22,18 +22,21 @@ public class tpIntegrador {
 		        List <Pronosticos> listaDePronosticos;
 		        
 		        
-		        
 		        try {
 		          
-		        	listaDePartidos = new CsvToBeanBuilder<Partidos>(new FileReader(args[0]))		                 
+		        	listaDePartidos = new CsvToBeanBuilder<Partidos>(new FileReader(args[0]))
 		                    .withType(Partidos.class)
 		                    .build()
 		                    .parse();
 		          
+
+		        	
 		           for (Partidos partidos : listaDePartidos) {
 		                String ganador = null;
-		                String ganador1 = null;	                
-		                       
+		                String ganador1 = null;
+		            
+		                
+                      if ((partidos.getGolesEquipoA() % 1 == 0) && (partidos.getGolesEquipoB() % 1 == 0)) {
 		                if (partidos.getGolesEquipoA() > partidos.getGolesEquipoB()) {
 		                	ganador = partidos.getEquipoA();
 		                    System.out.println ("El ganador es: " + ganador);
@@ -44,44 +47,117 @@ public class tpIntegrador {
 		                	ganador = partidos.getEquipoA();
 		                	ganador1 = partidos.getEquipoB();
 		                    System.out.println ("Empate entre: " + ganador + " y " + ganador1);
+		                   
 		            }
-		            }
-		            
-			        listaDePronosticos = new CsvToBeanBuilder<Pronosticos>(new FileReader(args[1]))		        		   
+		           }
+		           }  
+                      
+			        listaDePronosticos = new CsvToBeanBuilder<Pronosticos>(new FileReader(args[1]))	        		
 			        		 .withType(Pronosticos.class)
 			        		 .build()
 			        		 .parse(); 
 		            
-			        int sumatoria = 0;
-			        
+			        int sumatoriaMariana = 0;
+			        int sumatoriaPedro = 0;
+			        String persona1 = null;
 			        
 			        for (Pronosticos pronosticos : listaDePronosticos) {
 			        	int a = 0;
-			        	int b = 0;
-			        	int c = 0;
-			        	int d = 0;
-			        	
+			      
+			        	if (pronosticos.getPersona().equals("Mariana")) {
+			        		persona1 = "Mariana";
 	                     if (pronosticos.getGanaEquipoB().equals("x") && pronosticos.getEquipoA().equals("Argentina") && pronosticos.getEquipoB().equals("Uruguay")) {
 	                           	a = a +1;
-	                           	sumatoria = sumatoria + a;
+	                           	sumatoriaMariana = sumatoriaMariana + a;
+	                           	a = 0;
+	                           	
 	                      
 	                        } else if (pronosticos.getGanaEquipoB().equals("x") && pronosticos.getEquipoA().equals("Brasil") && pronosticos.getEquipoB().equals("Chile")) {
-	                            b = b +1;
-	                            sumatoria = sumatoria + b;
+	                            a = a +1;
+	                            sumatoriaMariana = sumatoriaMariana + a;
+	                            a = 0;
 	                            
 	                        } else if (pronosticos.getEmpateEquipos().equals("x") && pronosticos.getEquipoA().equals("Ecuador") && pronosticos.getEquipoB().equals("Bolivia")) {
-	                            c = c + 1;
-	                            sumatoria = sumatoria + c;
+	                            a = a + 1;
+	                            sumatoriaMariana = sumatoriaMariana + a;
+	                            a = 0;
 	                            
 	                        } else if (pronosticos.getGanaEquipoA().equals("x") && pronosticos.getEquipoA().equals("Venezuela") && pronosticos.getEquipoB().equals("Colombia")) {
-			                    d = d + 1;
-			                    sumatoria = sumatoria + d;
+			                    a = a + 1;
+			                    sumatoriaMariana = sumatoriaMariana + a;
+			                    a = 0;
 			                    
+	                        } else if (pronosticos.getGanaEquipoA().equals("x") && pronosticos.getEquipoA().equals("Argentina") && pronosticos.getEquipoB().equals("Colombia")) {
+	                           	a = a +1;
+	                           	sumatoriaMariana = sumatoriaMariana + a;
+	                           	a = 0;
+	                           	
+	                        } else if (pronosticos.getGanaEquipoA().equals("x") && pronosticos.getEquipoA().equals("Venezuela") && pronosticos.getEquipoB().equals("Uruguay")) {
+	                            a = a +1;
+	                            sumatoriaMariana = sumatoriaMariana + a;
+	                            a = 0;
+	                            
+	                        } else if (pronosticos.getGanaEquipoB().equals("x") && pronosticos.getEquipoA().equals("Brasil") && pronosticos.getEquipoB().equals("Bolivia")) {
+	                            a = a + 1;
+	                            sumatoriaMariana = sumatoriaMariana + a;
+	                            a = 0;
+	                            
+	                        } else if (pronosticos.getEmpateEquipos().equals("x") && pronosticos.getEquipoA().equals("Ecuador") && pronosticos.getEquipoB().equals("Chile")) {
+			                    a = a + 1;
+			                    sumatoriaMariana = sumatoriaMariana + a;
+			                    a = 0;
+	                        }
+	                        }
+			        	
+			        	if (pronosticos.getPersona().equals("Pedro")) {
+			        		persona1 = "Pedro";
+	                     if (pronosticos.getGanaEquipoB().equals("x") && pronosticos.getEquipoA().equals("Argentina") && pronosticos.getEquipoB().equals("Uruguay")) {
+	                           	a = a +1;
+	                           	sumatoriaPedro = sumatoriaPedro + a;
+	                           	a = 0;
+	                           	
+	                      
+	                        } else if (pronosticos.getGanaEquipoB().equals("x") && pronosticos.getEquipoA().equals("Brasil") && pronosticos.getEquipoB().equals("Chile")) {
+	                            a = a +1;
+	                            sumatoriaPedro = sumatoriaPedro + a;
+	                            a = 0;
+	                            
+	                        } else if (pronosticos.getEmpateEquipos().equals("x") && pronosticos.getEquipoA().equals("Ecuador") && pronosticos.getEquipoB().equals("Bolivia")) {
+	                            a = a + 1;
+	                            sumatoriaPedro = sumatoriaPedro + a;
+	                            a = 0;
+	                            
+	                        } else if (pronosticos.getGanaEquipoA().equals("x") && pronosticos.getEquipoA().equals("Venezuela") && pronosticos.getEquipoB().equals("Colombia")) {
+			                    a = a + 1;
+			                    sumatoriaPedro = sumatoriaPedro + a;
+			                    a = 0;
+			                    
+	                        } else if (pronosticos.getGanaEquipoA().equals("x") && pronosticos.getEquipoA().equals("Argentina") && pronosticos.getEquipoB().equals("Colombia")) {
+	                           	a = a +1;
+	                           	sumatoriaPedro = sumatoriaPedro + a;
+	                           	a = 0;
+	                           	
+	                        } else if (pronosticos.getGanaEquipoA().equals("x") && pronosticos.getEquipoA().equals("Venezuela") && pronosticos.getEquipoB().equals("Uruguay")) {
+	                            a = a +1;
+	                            sumatoriaPedro = sumatoriaPedro + a;
+	                            a = 0;
+	                            
+	                        } else if (pronosticos.getGanaEquipoB().equals("x") && pronosticos.getEquipoA().equals("Brasil") && pronosticos.getEquipoB().equals("Bolivia")) {
+	                            a = a + 1;
+	                            sumatoriaPedro = sumatoriaPedro + a;
+	                            a = 0;
+	                            
+	                        } else if (pronosticos.getEmpateEquipos().equals("x") && pronosticos.getEquipoA().equals("Ecuador") && pronosticos.getEquipoB().equals("Chile")) {
+			                    a = a + 1;
+			                    sumatoriaPedro = sumatoriaPedro + a;
+			                    a = 0;
+	                        }
 	                        }
 			        }
 		            
-	                System.out.println("El puntaje obtenido por pronósticos correctos es: " + sumatoria);
-		            
+	                System.out.println("El puntaje obtenido por pronósticos correctos de " + persona1 + " es: " + sumatoriaMariana);
+	                System.out.println("El puntaje obtenido por pronósticos correctos de " + persona1 + " es: " + sumatoriaPedro);
+	                
 		        } catch (IOException e) {
 		            e.printStackTrace();
 		        }
